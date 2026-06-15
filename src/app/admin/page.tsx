5a1a364a70b5a2c83f07087c3a3580b9567ce991
Anasbd05 <anastrying05@gmail.com>
2026-06-15 13:20:58 +0100

    changes

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,17 +1,9 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client";
-import { BookmarkCheck, Clock, Layers2, MapPin, Search } from "lucide-react";
+import { Car, Clock, Layers2, MapPin, Search } from "lucide-react";
 import React, { useEffect, useState } from "react";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+
 import { createClient } from "@/utils/supabase/client";
 
 interface Order {
@@ -49,6 +41,11 @@ const page = () => {
   useEffect(() => {
     FetchOrders();
   }, []);
+
+  const [viewDetails, setViewDetails] = useState(false);
+
+  const [filter, setFilter] = useState("All");
+
   return (
     <section className="min-h-screen  py-10 bg-white ">
       <main className="px-8 w-full ">
@@ -62,103 +59,152 @@ const page = () => {
         </div>
         {/* table */}
 
-        <main className=" mt-8 rounded-md shadow-sm border-[1.5px] border-input bg-white p-10">
+        <main className=" mt-8 rounded-md shadow-sm border-2 border-input bg-white p-10">
           <div className=" flex flex-row justify-between w-full">
-            <div className=" shadow-sm py-1.5 gap-3.5.5 rounded-md flex items-center w-2/5 pr-2 pl-2.5 border border-input bg-transparent text-black text-sm ">
+            <div className=" shadow-sm py-1.5 gap-2.5.5 rounded-md flex items-center w-2/5 pr-2 pl-2.5 border border-input bg-transparent text-black text-sm ">
               <Search className="w-5 h-5  text-gray-400 " />
               <input
                 type="text"
-                placeholder="Search by name , Ville "
+                placeholder="Search by name , Ville... "
                 className=" outline-0 text-gray-700 font-medium w-full "
               />
             </div>
             <div className=" flex flex-row gap-6 ">
-              <Select>
-                <SelectTrigger className="w-full max-w-48">
-                  <BookmarkCheck className="w-5 h-5 text-gray-400 " />
-                  <SelectValue placeholder="Select a status" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectLabel>Status</SelectLabel>
-                    <SelectItem
-                      className=" focus:bg-green-300 "
-                      value="Complete"
-                    >
-                      Complete
-                    </SelectItem>
-                    <SelectItem
-                      className=" focus:bg-orange-300 "
-                      value="Pending"
-                    >
-                      Pending
-                    </SelectItem>
-                    <SelectItem className=" focus:bg-red-300 " value="Canceled">
-                      Canceled
-                    </SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-              {/* <input
-                type="date"
-                className="w-full rounded-md pr-2 pl-2.5 border border-input bg-transparent text-black text-sm focus:outline-none"
-              /> */}
+              <button
+                onClick={() => setFilter("All")}
+                className={` cursor-pointer py-2 px-6 rounded-lg text-sm font-medium border-[1.5px] transition-all hover:border-sky-500 ${
+                  filter === "All" && "bg-sky-300 border-none  "
+                } `}
+              >
+                All
+              </button>
+              <button
+                onClick={() => setFilter("Completed")}
+                className={` cursor-pointer py-2 px-6 rounded-lg text-sm font-medium border-[1.5px] transition-all hover:border-emerald-500 ${
+                  filter === "Completed" && "bg-emerald-300 border-none "
+                } `}
+              >
+                Completed
+              </button>
+              <button
+                onClick={() => setFilter("Pending")}
+                className={` cursor-pointer py-2 px-6 rounded-lg text-sm font-medium border-[1.5px] transition-all hover:border-amber-500 ${
+                  filter === "Pending" && "bg-amber-300 border-none "
+                } `}
+              >
+                Pending
+              </button>
+              <button
+                onClick={() => setFilter("Rejected")}
+                className={` cursor-pointer py-2 px-6 rounded-lg text-sm font-medium border-[1.5px] transition-all hover:border-red-500 ${
+                  filter === "Rejected" && "bg-red-300 border-none "
+                } `}
+              >
+                Rejected
+              </button>
             </div>
           </div>
 
-          <main className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-6 ">
+          <main className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-10 ">
             {Orders.map((order, index) => (
               <div
                 key={index}
-                className=" rounded-md py-2 px-4 border-[1.5px] border-input bg-transparent "
+                className=" bg-white relative rounded-3xl py-3 px-4 border-[2.5px] border-sky-100"
               >
-                <div className=" flex flex-row justify-between mt-4 ">
-                  <h2 className=" text-lg font-semibold">{order.full_name}</h2>
-                  <span className=" text-muted-foreground font-medium text-[17px]   ">
-                    #{order.id}
-                  </span>
-                </div>
-                <div className=" flex flex-row gap-3.5 mt-4 items-center  ">
-                  <Clock className="w-4 h-4 font-medium " />
-                  <span className=" text-muted-foreground font-medium text-[17px]   ">
-                    {order.date} , {order.heure}
-                  </span>
-                </div>
-                <div className=" flex flex-row gap-3.5 mt-4 items-center  ">
-                  <MapPin className="w-4 h-4 font-medium " />
-                  <span className=" text-muted-foreground font-medium text-[17px]   ">
-                    {order.ville}
-                  </span>
-                </div>
-                <div className=" flex flex-row gap-3.5 mt-4 items-center  ">
-                  <Layers2 className="w-4 h-4 font-medium " />
-                  <span className=" text-muted-foreground font-medium text-[17px]   ">
-                    {order.category}
-                  </span>
-                </div>
-                <div className=" flex flex-row gap-3.5 mt-4   ">
-                  <span className="w-4 h-4">🚗</span>
-                  <span className=" text-muted-foreground font-medium text-[17px]   ">
-                    {order.brand}
-                  </span>
-                </div>
-                {/* devider */}
-                <div className="border-t border-dashed border-gray-400 my-4" />
-                <div className=" flex flex-row justify-between">
-                  <h2 className=" text-lg font-bold ">Order </h2>
-                  <p className=" text-sky-500 font-semibold ">
-                    {order.service}
-                  </p>
-                </div>
-                {/* confirm / reject */}
-                <div className=" flex flex-row w-full gap-6 mt-4 mb-2 ">
-                  <button className=" hover:opacity-80 cursor-pointer w-2/4 bg-emerald-600 rounded-md py-2.5 text-white font-bold   ">
-                    Confirm
+                {viewDetails === false ? (
+                  <button
+                    onClick={() => setViewDetails(true)}
+                    className=" cursor-pointer absolute top-[-1.5px] right-[-1.5px] rounded-tr-3xl bg-sky-300 px-4 py-3 text-center text-xs font-bold text-gray-900 uppercase transition hover:bg-sky-400"
+                  >
+                    View details
                   </button>
-                  <button className=" hover:opacity-80 cursor-pointer w-2/4 bg-red-600  rounded-md py-2.5 text-white font-bold  ">
-                    Reject
+                ) : (
+                  <button
+                    onClick={() => setViewDetails(false)}
+                    className=" cursor-pointer absolute top-[-1.5px] right-[-1.5px] rounded-tr-3xl bg-sky-300 px-4 py-3 text-center text-xs font-bold text-gray-900 uppercase transition hover:bg-sky-400"
+                  >
+                    Retour
                   </button>
-                </div>
+                )}
+                <h2 className=" text-lg mt-2 mb-6 font-semibold">
+                  {order.full_name}
+                </h2>
+                {viewDetails === false ? (
+                  <main>
+                    <div className=" flex items-center gap-3 py-2  ">
+                      <Clock className="w-4 h-4 text-sky-500" />
+                      <span className="text-sm font-medium text-slate-700">
+                        {order.date} • {order.heure}
+                      </span>
+                    </div>
+                    <div className=" flex items-center gap-3 py-2  ">
+                      <MapPin className="w-4 h-4 text-emerald-500" />
+                      <span className="text-sm font-medium text-slate-700">
+                        {order.ville}
+                      </span>
+                    </div>
+                    <div className=" flex items-center gap-3 py-2  ">
+                      <Layers2 className="w-4 h-4 text-violet-500" />
+                      <span className="text-sm font-medium text-slate-700">
+                        {order.category}
+                      </span>
+                    </div>
+                    <div className=" flex items-center gap-3 py-2  ">
+                      <Car className="w-4 h-4 text-orange-500" />
+                      <span className="text-sm font-medium text-slate-700">
+                        {order.brand}
+                      </span>
+                    </div>
+                    {/* devider */}
+                    <div className="border-t border-dashed border-gray-300 my-4" />
+                    <div className=" flex flex-row justify-between">
+                      <h2 className=" text-lg font-bold ">Order </h2>
+                      <p className=" text-sky-500 font-semibold ">
+                        {order.service}
+                      </p>
+                    </div>
+                    {/* confirm / reject */}
+                    <div className=" flex flex-row w-full mt-4 mb-2 gap-6 ">
+                      <button className=" hover:opacity-80 cursor-pointer w-2/4 bg-green-100 rounded-md py-2 text-green-700 font-medium   ">
+                        Confirm
+                      </button>
+                      <button className=" hover:opacity-80 cursor-pointer w-2/4 bg-red-100  rounded-md py-2 text-red-600  font-medium  ">
+                        Reject
+                      </button>
+                    </div>
+                  </main>
+                ) : (
+                  <main>
+                    <div className="space-y-2.5">
+                      <div className="flex flex-col gap-1">
+                        <span className="text-xs font-semibold text-slate-400 uppercase tracking-wider">
+                          Phone Number
+                        </span>
+                        <span className="text-slate-800 font-medium text-sm bg-slate-50 px-3 py-2 rounded-md border border-slate-100">
+                          0{order.phone_number}
+                        </span>
+                      </div>
+
+                      <div className="flex flex-col gap-1">
+                        <span className="text-xs font-semibold text-slate-400 uppercase tracking-wider">
+                          Address
+                        </span>
+                        <span className="text-slate-800 font-medium text-sm bg-slate-50 px-3 py-2 rounded-md border border-slate-100">
+                          {order.address}
+                        </span>
+                      </div>
+                    </div>
+                    {/* devider */}
+                    <div className="border-t border-dashed border-gray-300 my-4" />
+                    <div className=" flex flex-row justify-between">
+                      <h2 className=" text-lg font-bold ">Total</h2>
+                      <p className=" text-emerald-500 font-medium">220 MAD</p>
+                    </div>
+                    <button className=" hover:opacity-80 cursor-pointer w-full bg-red-500 mt-4 mb-2 rounded-md py-2 text-white font-medium  ">
+                      Remove Demand
+                    </button>
+                  </main>
+                )}
               </div>
             ))}
           </main>
